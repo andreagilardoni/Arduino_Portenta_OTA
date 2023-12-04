7434fb1277b4efa73138334d4c7c6892b7266e52
Andrea Gilardoni <a.gilardoni@arduino.cc>
2023-12-04 15:55:56 +0100

    moved header outside of decompress function

--- src/decompress/utility.cpp
+++ src/decompress/utility.cpp
@@ -89,6 +89,37 @@ uint32_t crc_update(uint32_t crc, const void * data, size_t data_len)
    MAIN
  **************************************************************************************/
 
+union HeaderVersion
+{
+  struct __attribute__((packed))
+  {
+    uint32_t header_version    :  6;
+    uint32_t compression       :  1;
+    uint32_t signature         :  1;
+    uint32_t spare             :  4;
+    uint32_t payload_target    :  4;
+    uint32_t payload_major     :  8;
+    uint32_t payload_minor     :  8;
+    uint32_t payload_patch     :  8;
+    uint32_t payload_build_num : 24;
+  } field;
+  uint8_t buf[sizeof(field)];
+  static_assert(sizeof(buf) == 8, "Error: sizeof(HEADER.VERSION) != 8");
+};
+
+union OTAHeader
+{
+  struct __attribute__((packed))
+  {
+    uint32_t len;
+    uint32_t crc32;
+    uint32_t magic_number;
+    HeaderVersion hdr_version;
+  } header;
+  uint8_t buf[sizeof(header)];
+  static_assert(sizeof(buf) == 20, "Error: sizeof(HEADER) != 20");
+};
+
 int Arduino_Portenta_OTA::download(const char * url, bool const is_https, MbedSocketClass * socket)
 {
   return socket->download((char *)url, UPDATE_FILE_NAME_LZSS, is_https);
@@ -103,36 +134,7 @@ int Arduino_Portenta_OTA::decompress()
   /* For UPDATE.BIN.LZSS - LZSS compressed binary files. */
   FILE* update_file = fopen(UPDATE_FILE_NAME_LZSS, "rb");
 
-  union HeaderVersion
-  {
-    struct __attribute__((packed))
-    {
-      uint32_t header_version    :  6;
-      uint32_t compression       :  1;
-      uint32_t signature         :  1;
-      uint32_t spare             :  4;
-      uint32_t payload_target    :  4;
-      uint32_t payload_major     :  8;
-      uint32_t payload_minor     :  8;
-      uint32_t payload_patch     :  8;
-      uint32_t payload_build_num : 24;
-    } field;
-    uint8_t buf[sizeof(field)];
-    static_assert(sizeof(buf) == 8, "Error: sizeof(HEADER.VERSION) != 8");
-  };
-
-  union
-  {
-    struct __attribute__((packed))
-    {
-      uint32_t len;
-      uint32_t crc32;
-      uint32_t magic_number;
-      HeaderVersion hdr_version;
-    } header;
-    uint8_t buf[sizeof(header)];
-    static_assert(sizeof(buf) == 20, "Error: sizeof(HEADER) != 20");
-  } ota_header;
+  OTAHeader ota_header;
   uint32_t crc32, bytes_read;
   uint8_t crc_buf[128];
 
